264b829110cba7e621e7a3b3064e03a1cab7831c
PabloMesaValladares <pmesavalladares@gmail.com>
2023-08-07 17:25:41 +0200

    Movimiento y Camaras

---
+++ Assets/Scripts/MovementPlayer.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+using UnityEngine.TextCore.Text;
+using UnityEngine.Windows;
+
+/*
+public class MovementPlayer : MonoBehaviour
+{
+    private UnityEngine.InputSystem.PlayerInput playerControllerAssets;
+    private InputAction move;
+
+    private Rigidbody _rb
+        ;
+    [SerializeField]
+    private Vector3 velocity;
+    private Vector2 input;
+    [SerializeField]
+    private float jumpForce = 5f;
+    [SerializeField]
+    private float maxSpeed = 5f;
+    private Vector3 forceDirection = Vector3.zero;
+
+    [SerializeField]
+    private Camera playerCamera;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        _rb = this.GetComponent<Rigidbody>();
+        playerControllerAssets = new UnityEngine.InputSystem.PlayerInput();
+        move = playerControllerAssets.actions["Move"];
+    }
+
+    private void OnEnable()
+    {
+
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+
+}
+    */
